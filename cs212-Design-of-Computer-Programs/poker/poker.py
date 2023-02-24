35e3bf3d2f22f1776f86e2777cc387a382f8b7c0
Congcong Hu <hucongcong@kos-MacBook-puro.local>
2023-02-24 14:24:43 -0800

    refactor poker

--- cs212-Design-of-Computer-Programs/poker/poker.py
+++ cs212-Design-of-Computer-Programs/poker/poker.py
@@ -22,72 +22,35 @@ def allmax(iterable, key=None):
 
 
 def hand_rank(hand):
-    """Return a value indicating the ranking of a hand."""
-    ranks = card_ranks(hand)
-    if straight(ranks) and flush(hand):
-        return 8, max(ranks)
-    elif kind(4, ranks):
-        return 7, kind(4, ranks), kind(1, ranks)
-    elif kind(3, ranks) and kind(2, ranks):
-        return 6, kind(3, ranks), kind(2, ranks)
-    elif flush(hand):
-        return 5, ranks
-    elif straight(ranks):
-        return 4, max(ranks)
-    elif kind(3, ranks):
-        return 3, kind(3, ranks), ranks
-    elif two_pair(ranks):
-        return 2, two_pair(ranks), ranks
-    elif kind(2, ranks):
-        return 1, kind(2, ranks), ranks
-    else:
-        return 0, ranks
-
-
-def card_ranks(cards):
     """
-    returns an ORDERED list of the ranks in a hand (where the order goes from highest to lowest rank).
+    Return a value indicating the ranking of a hand and the ranks to break a tie
+    Examples:
+    Straight Flush: "6C 7C 8C 9C TC" => (9, (10, 9, 8, 7, 6))
+    4 of a kind: "9D 9H 9S 9C 7D" => (7, (9, 7))
+    Full House: "TD TC TH 7C 7D" => (6, (10, 7))
+    """
+    counts, ranks = unzip(group(hand))
+    straight = ranks[0] - ranks[-1] == 4 and len(ranks) == 5
+    flush = len(set([s for r, s in hand])) == 1
+    return max(count_rankings[counts], straight * 4 + 5 * flush), ranks
+
+
+count_rankings = {(5, ): 10, (4, 1): 7, (3, 2): 6, (3, 1, 1): 3,
+                  (2, 2, 1): 2, (2, 1, 1, 1): 1, (1, 1, 1, 1, 1): 0}
+
+
+def group(cards):
+    """
+    returns a list of ordered pairs of counts and ranks. Highest count first, then highest rank first.
+    e.g. '7 T 7 9 7' => [(count, rank), ...]: [(3, 7), (1, 10), (1, 9)]
     """
     ranks = ['--23456789TJQKA'.index(r) for r, s in cards]
-    ranks.sort(reverse=True)
-    return [5, 4, 3, 2, 1] if ranks == [14, 5, 4, 3, 2] else ranks
+    if ranks == [14, 5, 4, 3, 2]: ranks = [5, 4, 3, 2, 1]  # when straight, A is seen as 1
+    groups = [(ranks.count(x), x) for x in set(ranks)]
+    return sorted(groups, reverse=True)
 
 
-def straight(ranks):
-    """
-    Return True if the ordered ranks form a 5-card straight.
-    ranks is the ordered ranks of the hand from the highest rank to the lowest
-    """
-    for i in range(len(ranks) - 1):
-        if ranks[i] - ranks[i + 1] != 1:
-            return False
-    return True
-
-
-def flush(hand):
-    """Return True if all the cards have the same suit."""
-    suit = [s for r, s in hand]
-    return len(set(suit)) == 1
-
-
-def kind(n, ranks):
-    """Return the first rank that this hand has exactly n of.
-    Return None if there is no n-of-a-kind in the hand."""
-    for r in ranks:
-        if ranks.count(r) == n:
-            return r
-    return None
-
-
-def two_pair(ranks):
-    """If there are two pair, return the two ranks as a
-    tuple: (highest, lowest); otherwise return None."""
-    highest = kind(2, ranks)
-    lowest = kind(2, ranks[::-1])
-    if highest and highest != lowest:
-        return highest, lowest
-    else:
-        return None
+def unzip(pairs): return zip(*pairs)
 
 
 def test():
@@ -96,31 +59,15 @@ def test():
     fk = "9D 9H 9S 9C 7D".split()  # Four of a Kind
     fh = "TD TC TH 7C 7D".split()  # Full House
     tp = "5S 5D 9H 9C 6S".split()  # Two pairs
-    fkranks = card_ranks(fk)
-    tpranks = card_ranks(tp)
-    # test straight and flush
-    assert straight([9, 8, 7, 6, 5])
-    assert not straight([9, 8, 8, 6, 5])
-    assert flush(sf)
-    assert not flush(fk)
-
-    # test card_ranks
-    assert card_ranks(sf) == [10, 9, 8, 7, 6]
-    assert card_ranks(fk) == [9, 9, 9, 9, 7]
-    assert card_ranks(fh) == [10, 10, 10, 7, 7]
-    # test kind
-    assert kind(4, fkranks) == 9
-    assert not kind(3, fkranks)
-    assert not kind(2, fkranks)
-    assert kind(1, fkranks) == 7
-    # test 2 pair
-    assert not two_pair(fkranks)
-    assert two_pair(tpranks) == (9, 5)
+    assert group(sf) == [(1, 10), (1, 9), (1, 8), (1, 7), (1, 6)]
+    assert group(fk) == [(4, 9), (1, 7)]
+    assert group(fh) == [(3, 10), (2, 7)]
+    assert group(tp) == [(2, 9), (2, 5), (1, 6)]
 
     # test that hand_rank works
-    assert hand_rank(sf) == (8, 10)
-    assert hand_rank(fk) == (7, 9, 7)
-    assert hand_rank(fh) == (6, 10, 7)
+    assert hand_rank(sf) == (9, (10, 9, 8, 7, 6))
+    assert hand_rank(fk) == (7, (9, 7))
+    assert hand_rank(fh) == (6, (10, 7))
 
     assert poker([sf, fk, fh]) == [sf]
     # four of a kind (fk) vs. full house (fh) returns fk.
